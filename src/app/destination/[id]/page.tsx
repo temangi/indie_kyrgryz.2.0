91542ef8de3b93139c99aab9be874d151590805e
temmirlan <temmirlan@MacBook-Pro--temanbot.local>
2026-02-09 10:37:37 +0600

    fix: layout schema

--- src/app/destination/[id]/page.tsx
+++ src/app/destination/[id]/page.tsx
@@ -1,4 +1,37 @@
 import DestinationPage from "@/src/pages/destination/page";
+import { Metadata } from "next";
+import { destinationTourInfo } from "@/src/widgets/DestinationTourList/constants/constansts";
+
+type Props = {
+  params: Promise<{ id: string }>; 
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const currentTour = destinationTourInfo[Number(id) - 1];
+
+  if (!currentTour) {
+    return {
+      title: "Tour Not Found | Indie Kyrgyz Travel",
+    };
+  }
+
+  return {
+    title: `${currentTour.title} | Indie Kyrgyz Travel`,
+    description: currentTour.desc,
+    openGraph: {
+      title: currentTour.title,
+      description: currentTour.desc,
+      images: [
+        {
+          url: "/og.jpg", 
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
 
 export default function Destination() {
     return <DestinationPage />;
